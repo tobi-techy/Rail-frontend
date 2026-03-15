c74d223feea2bb7c1a487d8351933eeb4e2eacfe
Tobi <omotadetobiloba@gmail.com>
2026-03-15 21:59:55 +0100

    feat: replace intro mp4 videos with static images

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,7 +1,7 @@
-import onBoard1 from './onboard1.mp4';
-import onBoard2 from './onboard2.mp4';
-import onBoard3 from './onboard3.mp4';
-import onBoard4 from './onboard4.mp4';
+import onBoard1 from './Image@4x-1.png';
+import onBoard2 from './Image@4x-2.png';
+import onBoard3 from './Image@4x-3.png';
+import onBoard4 from './Image@4x-4.png';
 import splashVideo from './splash.mp4';
 import bell from './bell.png';
 import gear from './gear.png';
@@ -9,4 +9,15 @@ import checkmark from './checkmark.png';
 import cards from './cards.png';
 import avatar from './avatar.png';
 
-export { onBoard1, onBoard2, onBoard3, onBoard4, splashVideo, bell, gear, checkmark, cards, avatar };
+export {
+  onBoard1,
+  onBoard2,
+  onBoard3,
+  onBoard4,
+  splashVideo,
+  bell,
+  gear,
+  checkmark,
+  cards,
+  avatar,
+};
